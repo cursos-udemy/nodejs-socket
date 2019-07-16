dd8f7c5db5b66bf976dca09bbf937798643453b1
gwfernandez <gwfernandez@gmail.com>
2019-07-16 18:13:38 -0300

    commit inicial

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (client) => {
+
+    client.emit('established-connection', {
+        user: 'Administrador',
+        connection: 'ok',
+        timestamp: new Date().getTime(),
+        message: 'Aplicacion lista para usar!!'
+    });
+
+    client.on('disconnect', () => {
+        console.log('El usuario se desconecto');
+    });
+
+    client.on('login', (data) => {
+        console.log(`Bienvenido ${data.username}`);
+    });
+});
+
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
+
+server.listen(port, (err) => {
+    if (err) throw new Error(err);
+    console.log(`Servidor corriendo en puerto ${port}`);
+});

f6e9c54785891a631647ac81c1a33c2d9b84550d
gwfernandez <gwfernandez@gmail.com>
2019-07-16 22:19:36 -0300

    broadcast, mensajes para todos los clientes

--- server/server.js
+++ server/server.js
@@ -2,36 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-const port = process.env.PORT || 3000;
-
-io.on('connection', (client) => {
-
-    client.emit('established-connection', {
-        user: 'Administrador',
-        connection: 'ok',
-        timestamp: new Date().getTime(),
-        message: 'Aplicacion lista para usar!!'
-    });
-
-    client.on('disconnect', () => {
-        console.log('El usuario se desconecto');
-    });
-
-    client.on('login', (data, callback) => {
-        if (data.username) {
-            console.log(`El usuario ${data.username} ingreso a al aplicacion`);
-            callback(`Bienvenido ${data.username}`);
-        } else {
-            callback(`Por favor ingrese sus credenciales`);
-        }
-    });
-});
+module.exports.io = require('socket.io')(server);
+require('./sockets/app-socket');
 
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
+const port = process.env.PORT || 3000;
 server.listen(port, (err) => {
     if (err) throw new Error(err);
     console.log(`Servidor corriendo en puerto ${port}`);
